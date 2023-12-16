14f4d1ca226b93814834d7e3076f59db4edc8fe3
Tatiana Cifuentes Gaitán <taciga30@gmail.com>
2023-12-16 03:20:55 -0500

    Test corregidos

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,11 +1,14 @@
-const { mdLinks } = require('../src/index');
-const { routeIsAbsolute, relativeToAbsolute } = require('../src/function');
+const { mdLinks } = require("../src/index");
+const {
+  routeIsAbsolute,
+  relativeToAbsolute,
+  readMd,
+} = require("../src/function");
 
-describe('mdLinks', () => {
-
-  it('Debería devolver una promesa', () => {
-    const resultado = mdLinks('./README.md');
-    expect(typeof (resultado.then)).toBe('function');
+describe("mdLinks", () => {
+  it("Debería devolver una promesa", () => {
+    const resultado = mdLinks("./README.md");
+    expect(typeof resultado.then).toBe("function");
   });
 
   it('Debería rechazar cuando el path no existe', () => {
@@ -14,15 +17,19 @@ describe('mdLinks', () => {
     })
   });
 
-  it('Debería devolver false cuando es relativa', () => {
-    return routeIsAbsolute('docs/01-milestone.md').catch((error)=>{
-      expect(error).toBe('false');
-    })
+  it("Debería devolver false cuando es relativa", () => {
+    const isAbsolute = routeIsAbsolute("test/ReadMePrueba.md");
+    expect(isAbsolute).toBe(false);
   });
 
-  it('Debería rechazar cuando el path no existe', () => {
-      expect(relativeToAbsolute('docs/01-milestone.md')).toBe('C:/Users/tacig/OneDrive/TATIS/Documentos/Tatis/Educación/Cursos y Capacitaciones/Laboratoria/Bootcamp Desarrollo Web/Proyectos/Proyecto 4 Md Links/DEV011-md-links/docs/01-milestone.md');
-    })
+  // it('Debería rechazar cuando el path no existe', () => {
+  //     expect(relativeToAbsolute('docs/01-milestone.md')).toBe('C:/Users/tacig/OneDrive/TATIS/Documentos/Tatis/Educación/Cursos y Capacitaciones/Laboratoria/Bootcamp Desarrollo Web/Proyectos/Proyecto 4 Md Links/DEV011-md-links/docs/01-milestone.md');
+  //   })
 
-
+  it("Debe traer la data", () => {
+    const resultado =
+      "[Documentación oficial](https://nodejs.org/es/about/),[Fazt en YouTube](https://www.youtube.com/watch?v=WgSc1nv_4Gw),[Asíncronía en js](https://carlosazaustre.es/manejando-la-asincronia-en-javascript),[mozilla.org](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_expressions)";
+    const obteinData = readMd("test/ReadMePrueba.md");
+    expect(obteinData).resolves.toBe(resultado);
+  });
 });
